9b1ec968dfd80c394251668cc06ecba24d68779d
vladi <v16012003@gmail.com>
2025-03-28 11:33:04 +0300

    Initial commit

---
+++ src/main/java/organization/family/orgfamily/OrgfamilyApplication.java
@@ -0,0 +1,13 @@
+package organization.family.orgfamily;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OrgfamilyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OrgfamilyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/organization/family/orgfamily/OrgfamilyApplicationTests.java
@@ -0,0 +1,13 @@
+package organization.family.orgfamily;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrgfamilyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
